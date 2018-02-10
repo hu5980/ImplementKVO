0002d59a6efaaa24ac6aea993a3cc7cd584a6258
胡光耀 <xinghen01@163.com>
2018-02-10 23:28:12 +0800

    runtime 知识备注

--- ImplementKVO/NSObject+KVO.m
+++ ImplementKVO/NSObject+KVO.m
@@ -131,14 +131,14 @@ static void kvo_setter(id self, SEL _cmd, id newValue)
     void (*objc_msgSendSuperCasted)(void *, SEL, id) = (void *)objc_msgSendSuper;
     
     // call super's setter, which is original class's setter method
-    objc_msgSendSuperCasted(&superclazz, _cmd, newValue);
+    objc_msgSendSuperCasted(&superclazz, _cmd, newValue); //用runtime的方法将新值赋给 属性
     
     // look up observers and call the blocks
     NSMutableArray *observers = objc_getAssociatedObject(self, (__bridge const void *)(kPGKVOAssociatedObservers));
     for (PGObservationInfo *each in observers) {
         if ([each.key isEqualToString:getterName]) {
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                each.block(self, getterName, oldValue, newValue);
+                each.block(self, getterName, oldValue, newValue); //block 回调
             });
         }
     }
@@ -158,8 +158,12 @@ static Class kvo_class(id self, SEL _cmd)
                 forKey:(NSString *)key
              withBlock:(PGObservingBlock)block
 {
+    //获取属性的set方法
     SEL setterSelector = NSSelectorFromString(setterForGetter(key));
+    //class_getInstanceMethod class_getClassMethod  的区别 https://www.jianshu.com/p/570d954bdacc
+    //class_getInstanceMethod 得到类的实例方法
     Method setterMethod = class_getInstanceMethod([self class], setterSelector);
+    //实例方法不存在 报异常错误
     if (!setterMethod) {
         NSString *reason = [NSString stringWithFormat:@"Object %@ does not have a setter for key %@", self, key];
         @throw [NSException exceptionWithName:NSInvalidArgumentException
@@ -169,10 +173,10 @@ static Class kvo_class(id self, SEL _cmd)
         return;
     }
     
-    Class clazz = object_getClass(self);
-    NSString *clazzName = NSStringFromClass(clazz);
+    Class clazz = object_getClass(self); //获取对象所在的类
+    NSString *clazzName = NSStringFromClass(clazz); //获取类的名字
     
-    // if not an KVO class yet
+    // if not an KVO class yet  如果不是 KVO的类 则创建一个  此处是为了防止多次创建 所以加了判断
     if (![clazzName hasPrefix:kPGKVOClassPrefix]) {
         clazz = [self makeKvoClassWithOriginalClassName:clazzName];
         object_setClass(self, clazz);
@@ -180,14 +184,16 @@ static Class kvo_class(id self, SEL _cmd)
     
     // add our kvo setter if this class (not superclasses) doesn't implement the setter?
     if (![self hasSelector:setterSelector]) {
-        const char *types = method_getTypeEncoding(setterMethod);
-        class_addMethod(clazz, setterSelector, (IMP)kvo_setter, types);
+        const char *types = method_getTypeEncoding(setterMethod); //获取setter 函数type encoding
+        class_addMethod(clazz, setterSelector, (IMP)kvo_setter, types); //添加setter 方法
     }
     
     PGObservationInfo *info = [[PGObservationInfo alloc] initWithObserver:observer Key:key block:block];
-    NSMutableArray *observers = objc_getAssociatedObject(self, (__bridge const void *)(kPGKVOAssociatedObservers));
+    NSMutableArray *observers = objc_getAssociatedObject(self, (__bridge const void *)(kPGKVOAssociatedObservers)); //用于获取关联对象
+    //关联对象不存在
     if (!observers) {
         observers = [NSMutableArray array];
+        //用于给对象添加关联对象，传入 nil 则可以移除已有的关联对象；
         objc_setAssociatedObject(self, (__bridge const void *)(kPGKVOAssociatedObservers), observers, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     }
     [observers addObject:info];
@@ -196,6 +202,7 @@ static Class kvo_class(id self, SEL _cmd)
 
 - (void)PG_removeObserver:(NSObject *)observer forKey:(NSString *)key
 {
+    ////用于获取关联对象
     NSMutableArray* observers = objc_getAssociatedObject(self, (__bridge const void *)(kPGKVOAssociatedObservers));
     
     PGObservationInfo *infoToRemove;
@@ -205,44 +212,53 @@ static Class kvo_class(id self, SEL _cmd)
             break;
         }
     }
-    
+    //从observers 移除关联对象
     [observers removeObject:infoToRemove];
 }
 
+/*
+ // 创建一个新类和元类
+ Class objc_allocateClassPair ( Class superclass, const char *name, size_t extraBytes );
+ // 销毁一个类及其相关联的类
+ void objc_disposeClassPair ( Class cls );
+ // 在应用中注册由objc_allocateClassPair创建的类
+ void objc_registerClassPair ( Class cls );
+ */
 
 - (Class)makeKvoClassWithOriginalClassName:(NSString *)originalClazzName
 {
-    NSString *kvoClazzName = [kPGKVOClassPrefix stringByAppendingString:originalClazzName];
-    Class clazz = NSClassFromString(kvoClazzName);
-    
+    NSString *kvoClazzName = [kPGKVOClassPrefix stringByAppendingString:originalClazzName];  //新类的名称
+    Class clazz = NSClassFromString(kvoClazzName); //获取类
+    //如果类存在 直接返回 ，否则创建新类
     if (clazz) {
         return clazz;
     }
     
     // class doesn't exist yet, make it
     Class originalClazz = object_getClass(self);
-    Class kvoClazz = objc_allocateClassPair(originalClazz, kvoClazzName.UTF8String, 0);
+    Class kvoClazz = objc_allocateClassPair(originalClazz, kvoClazzName.UTF8String, 0);  //创建一个新类和元类  （是self的一个子类）
     
     // grab class method's signature so we can borrow it
-    Method clazzMethod = class_getInstanceMethod(originalClazz, @selector(class));
-    const char *types = method_getTypeEncoding(clazzMethod);
-    class_addMethod(kvoClazz, @selector(class), (IMP)kvo_class, types);
+    Method clazzMethod = class_getInstanceMethod(originalClazz, @selector(class)); //获取原来类的class 方法
+    const char *types = method_getTypeEncoding(clazzMethod); //获取函数type encoding
+    class_addMethod(kvoClazz, @selector(class), (IMP)kvo_class, types); //将原来类的class 方法指向 新创建的子类
     
-    objc_registerClassPair(kvoClazz);
+    objc_registerClassPair(kvoClazz);//在应用中注册由objc_allocateClassPair创建的类
     
     return kvoClazz;
 }
 
-
+//判断当前类是否拥有这个方法
 - (BOOL)hasSelector:(SEL)selector
 {
     Class clazz = object_getClass(self);
     unsigned int methodCount = 0;
+    //获取当前class的所有方法
     Method* methodList = class_copyMethodList(clazz, &methodCount);
     for (unsigned int i = 0; i < methodCount; i++) {
         SEL thisSelector = method_getName(methodList[i]);
         if (thisSelector == selector) {
-            free(methodList);
+            free(methodList); //释放内存
             return YES;
         }
     }
